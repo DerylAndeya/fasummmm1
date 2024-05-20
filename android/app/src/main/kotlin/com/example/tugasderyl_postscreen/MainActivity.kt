af1e03349d9f1b874e1ed792a0c82e0fb11c199b
cintaaasf <161106281+cintaaasf@users.noreply.github.com>
2024-05-20 15:03:20 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tugasderyl_postscreen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugasderyl_postscreen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tugasderyl_postscreen/screens/post_screen.dart';
+import 'package:tugasderyl_postscreen/screens/signin_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key});
+
+  Future<void> signOut(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => SignInScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        actions: [
+          IconButton(
+            onPressed: () {
+              signOut(context);
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddPostScreen()),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: const Center(
+        child: Text('You have logged In'),
+      ),
+    );
+  }
+}
